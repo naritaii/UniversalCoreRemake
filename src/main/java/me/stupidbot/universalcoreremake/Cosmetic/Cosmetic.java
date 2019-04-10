bd2fb0767d8e1c84f7258e2ef93929d82d441353
Nicholas Satish Hosein <nicholashosein12@gmail.com>
2019-04-09 20:44:54 -0400

    pushing latest code

--- src/main/java/me/stupidbot/universalcoreremake/Cosmetic/Cosmetic.java
+++ src/main/java/me/stupidbot/universalcoreremake/Cosmetic/Cosmetic.java
@@ -1,4 +1,14 @@
 package me.stupidbot.universalcoreremake.Cosmetic;
 
 public class Cosmetic {
+
+    String getName() {
+        return null;
+    }
+
+    String getId() { return null; }
+
+    String getDescription() {
+        return null;
+    }
 }
