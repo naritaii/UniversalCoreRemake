4b76fcb5e16b22275d7f5d7e705819fb4b6a9bd4
Lily Hosein <nicholashosein12@gmail.com>
2019-11-24 21:07:17 -0500

    combat update bug fixes/typos

--- src/main/java/me/stupidbot/universalcoreremake/items/armor/WoolHelmet.java
+++ src/main/java/me/stupidbot/universalcoreremake/items/armor/WoolHelmet.java
@@ -9,7 +9,7 @@ public class WoolHelmet extends ItemStack {
     public WoolHelmet() {
         super(ItemMetadata.setMeta(ItemMetadata.setMeta(ItemMetadata.setMeta(ItemMetadata.setMeta(
                 ItemMetadata.setMeta(
-                        new ItemBuilder(Material.WOOL).name("&rWool Helmet").unbreakable(true)
+                        new ItemBuilder(Material.WOOL).name("&rWool Helmet")
                                 .lore("&7Health: &a+2")
                                 .lore("&7Defense: &a+1").build(),
                         "HEALTH", "2"),
--- src/main/java/me/stupidbot/universalcoreremake/items/weapons/MrMeatsCleaver.java
+++ src/main/java/me/stupidbot/universalcoreremake/items/weapons/MrMeatsCleaver.java
@@ -3,13 +3,14 @@ package me.stupidbot.universalcoreremake.items.weapons;
 import me.stupidbot.universalcoreremake.utilities.item.ItemBuilder;
 import me.stupidbot.universalcoreremake.utilities.item.ItemMetadata;
 import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 
 public class MrMeatsCleaver extends ItemStack {
     public MrMeatsCleaver() {
         super(ItemMetadata.setMeta(ItemMetadata.setMeta(
                 ItemMetadata.setMeta(
-                        new ItemBuilder(Material.GOLD_AXE).name("&aMr. Meat's Cleaver").unbreakable(true)
+                        new ItemBuilder(Material.GOLD_AXE).name("&aMr. Meat's Cleaver").unbreakable(true).flag(ItemFlag.values())
                                 .lore("&7Damage: &a+5-6")
                                 .lore("")
                                 .lore("&7&oGood for cutting flesh...").build(),
